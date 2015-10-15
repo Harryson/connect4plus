77bdb72056ed6e14a6cd37fd09b816ebf0422866
Markus Harr <maharr@htwg-konstanz.de>
2015-10-15 12:02:34 +0200

    immigrate SaveGame to Scala

---
+++ src/main/scala/connectfour/model/SaveGame.scala
@@ -0,0 +1,10 @@
+package connectfour.model
+
+import scala.beans.BeanProperty
+
+/**
+ * Created by maharr on 14.10.15.
+ */
+class SaveGame(@BeanProperty val saveGameName: String, @BeanProperty val gameField: GameField, @BeanProperty val player1: Player, @BeanProperty val player2: Player) {
+
+}
