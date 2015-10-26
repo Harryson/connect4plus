b31435fcac4d59b466ea81776bf684a007c268d3
Markus Harr <maharr@htwg-konstanz.de>
2015-10-26 16:44:13 +0100

    immigrate HighScoreController.java to HighScoreController.scala

---
+++ old/IController.java
@@ -0,0 +1,57 @@
+package connectfour.controller;
+
+import connectfour.model.GameField;
+import connectfour.model.Player;
+
+import java.util.List;
+
+public interface IController {
+	 void newGame();
+     void newGame(Player player, Player opponent);
+
+	 String getWinner();
+    
+     boolean gameHasStarted();
+    
+     boolean dropCoinWithSuccessFeedback(final int col);
+    
+     Player getPlayerOnTurn();
+    
+     boolean userHasWon();
+    
+     String getPlayerNameOnTurn();
+    
+     void setGameField(final GameField gameField);
+    
+     GameField getGameField();
+    
+     void undoStep();
+    
+     void redoStep();
+    
+     void setPlayer(final Player p);
+    
+     void setOpponend(final Player p);
+    
+    // Only for Support. This method sould not be used any more
+    // DEPRECATED!!!!!
+    // Since its bad to get an Array of Objects.
+     Player[] getPlayers();
+    
+     Player getPlayerAt(final int row, final int col);
+    
+     Player getOpponend();
+    
+     Player getPlayer();
+    
+     void useState(final GameField state);
+    
+     /**
+      * @param name Name must be unique, otherwise it will be overwritten.
+      */
+     void saveGame(String name);
+     
+     List<String> getAllSaveGameNames();
+     
+     void loadSaveGame(String saveGameName);
+}
