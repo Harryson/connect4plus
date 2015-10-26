92c7bc4ebb61f5c7d60ed5f6a6b775816fec2481
Stefano Di Martino <stefano.d@posteo.de>
2015-10-26 16:10:08 +0100

    StatusDisplay portiert

---
+++ src/main/scala/connectfour/ui/gui/swing/StatusDisplay.scala
@@ -0,0 +1,42 @@
+package connectfour.ui.gui.swing
+
+import connectfour.controller.IController;
+
+import javax.swing._
+import java.awt._
+
+class StatusDisplay(c: IController) extends JPanel {
+  private val status = new JLabel();
+  private val controller = c;
+
+  this.add(status)
+  this.setBackground(Color.WHITE)
+  this.showPlayerOnTurn()
+
+  def update {
+    if (controller.userHasWon()) {
+      showWinner()
+    } else {
+      showPlayerOnTurn()
+    }
+  }
+
+  private def setPlayersColor {
+    if (controller.getPlayerOnTurn() == controller.getPlayers()(0)) { // Player 1
+      status.setForeground(Color.RED)
+    } else {
+      status.setForeground(Color.BLUE)
+    }
+  }
+
+  private def showWinner() {
+    val winner = String.format("%s hat gewonnen!", controller.getWinner)
+    status.setText(winner)
+  }
+
+  private def showPlayerOnTurn() {
+    setPlayersColor
+    val playerOnTurn = String.format("Spieler %s ist dran", controller.getPlayerNameOnTurn())
+    status.setText(playerOnTurn)
+  }
+}
