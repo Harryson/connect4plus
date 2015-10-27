ac717815edecb72df157f6b14db14149c6c1a872
Stefano Di Martino <stefano.d@posteo.de>
2015-10-27 13:40:37 +0100

    ArrowMouseListener.scala hinzu

---
+++ src/main/scala/connectfour/ui/gui/swing/events/ArrowMouseListener.scala
@@ -0,0 +1,33 @@
+package connectfour.ui.gui.swing.events
+
+import connectfour.controller.IController
+import connectfour.util.observer.IObserver
+import connectfour.util.observer.Observable
+
+import java.awt.event.MouseEvent
+import java.awt.event.MouseListener
+
+class ArrowMouseListener(controller: IController, observer: IObserver, column: Int) extends Observable with MouseListener {
+  
+  addObserver(observer)
+
+  override def mouseReleased(e: MouseEvent) {
+    controller.dropCoinWithSuccessFeedback(this.column)
+  }
+
+  override def mouseClicked(e: MouseEvent) {
+    notifyObservers
+  }
+
+  override def mouseEntered(e: MouseEvent) {
+    notifyObservers
+  }
+
+  override def mouseExited(e: MouseEvent) {
+    notifyObservers
+  }
+
+  override def mousePressed(e: MouseEvent) {
+    notifyObservers
+  }
+}
