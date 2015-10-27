94fc4b595777e5e5f50dc50ac39a7f6351e729d8
Stefano Di Martino <stefano.d@posteo.de>
2015-10-27 15:43:27 +0100

    MouseColumnListener.scala hinzu

---
+++ src/main/scala/connectfour/ui/gui/swing/events/MouseColumnListener.scala
@@ -0,0 +1,26 @@
+package connectfour.ui.gui.swing.events
+
+import java.awt.event.MouseEvent
+import java.awt.event.MouseListener
+
+import connectfour.util.observer.IObserverWithArguments
+import connectfour.util.observer.ObservableWithArguments
+
+class MouseColumnObserver(observer: IObserverWithArguments, column: Int) extends ObservableWithArguments with MouseListener {
+  addObserver(observer)
+
+  override def mouseEntered(e: MouseEvent) {
+    notifyObservers(column)
+  }
+
+  override def mouseExited(e: MouseEvent) {
+    notifyObservers(column)
+  }
+
+  override def mouseReleased(e: MouseEvent) {
+  }
+  override def mouseClicked(e: MouseEvent) {
+  }
+  override def mousePressed(e: MouseEvent) {
+  }
+}
