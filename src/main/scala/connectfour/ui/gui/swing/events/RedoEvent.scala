a588e1869414e8db16d02182e6d6f1719bcbeb3a
Stefano Di Martino <stefano.d@posteo.de>
2015-10-27 15:26:43 +0100

    RedoEvent.scala hinzu

---
+++ src/main/scala/connectfour/ui/gui/swing/events/RedoEvent.scala
@@ -0,0 +1,14 @@
+package connectfour.ui.gui.swing.events
+
+import connectfour.controller.IController
+import connectfour.util.observer.IObserver
+
+import java.awt.event.MouseEvent
+
+class RedoEvent(controller: IController, observer: IObserver) extends EventAdapter(observer) {
+
+  override def  mousePressed(e: MouseEvent) {
+        controller.redoStep
+        notifyObservers
+    }
+}
