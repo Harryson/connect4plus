5e4f53c31204772afc2c302f8c98aec92de6b749
Stefano Di Martino <stefano.d@posteo.de>
2015-12-20 19:12:10 +0100

    Kommentare gelöscht

--- src/test/scala/connect4/model/mocks/MockController.scala
+++ src/test/scala/connect4/model/mocks/MockController.scala
@@ -2,9 +2,6 @@ package connect4.model.mocks
 
 import connectfour.controller.Connect4GameController
 
-/**
-  * Created by stefano on 20.12.15.
-  */
 class MockController(player1Name: String, player2Name: String) extends Connect4GameController(player1Name, player2Name) {
   gameField = new MockGameField(player1, player2)
 
--- src/test/scala/connect4/model/mocks/MockGameField.scala
+++ src/test/scala/connect4/model/mocks/MockGameField.scala
@@ -3,9 +3,6 @@ package connect4.model.mocks
 import connectfour.model.Connect4GameField
 import modelinterfaces.Player
 
-/**
-  * Created by stefano on 20.12.15.
-  */
 class MockGameField(player1: Player, player2: Player) extends Connect4GameField(player1, player2) {
 
   def dropCoin(column: Int, p: Player): Boolean = {
