03d15c35855ab49abce468948ad055b9c0a09258
Stefano Di Martino <stefano.d@posteo.de>
2015-12-20 19:26:03 +0100

    Computer-Test angefangen

--- src/test/scala/connect4/model/Connect4ComputerTest.scala
+++ src/test/scala/connect4/model/Connect4ComputerTest.scala
@@ -3,6 +3,51 @@ package connect4.model
 import connect4.model.mocks.MockBase
 
 class Connect4ComputerTest extends MockBase{
+   /*
+    *   0   1   2   3   4   5   6
+    * |___|___|___|___|___|___|___|
+    * |_X_|___|___|___|___|___|___|
+    * |_O_|___|___|___|_X_|___|___|
+    * |_X_|___|___|_O_|_O_|___|___|
+    * |_X_|___|_X_|_O_|_O_|_X_|_O_|
+    * |_X_|___|_X_|_O_|_O_|_O_|_X_|
+    *
+    */
 
+  "The gamefield" should
+    "return 'Hugo' as winner in this complex match" in {
+    reset
+    gameField.dropCoin(0, opponent)
+    gameField.dropCoin(0, opponent)
+    gameField.dropCoin(0, opponent)
+    gameField.dropCoin(0, player)
+    gameField.dropCoin(0, opponent)
+
+    gameField.dropCoin(2, opponent)
+    gameField.dropCoin(2, opponent)
+
+    gameField.dropCoin(3, player)
+    gameField.dropCoin(3, player)
+    gameField.dropCoin(3, player)
+
+    gameField.dropCoin(4, player)
+    gameField.dropCoin(4, player)
+    gameField.dropCoin(4, player)
+    gameField.dropCoin(4, opponent)
+
+    gameField.dropCoin(5, player)
+    gameField.dropCoin(5, opponent)
+
+    gameField.dropCoin(6, opponent)
+
+    if (controller.getPlayerOnTurn == player) {
+      controller.dropCoin(6)
+    } else {
+      gameField.dropCoin(6, player)
+      controller.dropCoinEventScala.dropCoin()
+    }
+
+    controller.getPlayerAt(3, 3) should be(opponent)
+  }
 
 }
