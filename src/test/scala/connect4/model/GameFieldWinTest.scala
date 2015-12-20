0fd66e58c04a863c14ad68baed4f01ab01eb33b7
Stefano Di Martino <stefano.d@posteo.de>
2015-12-20 19:03:00 +0100

    One test added

--- src/test/scala/connect4/model/GameFieldWinTest.scala
+++ src/test/scala/connect4/model/GameFieldWinTest.scala
@@ -84,4 +84,45 @@ class GameFieldWinTest extends FlatSpec with Matchers {
 
     gameField.getWinner should be("Boss")
   }
+
+  /*
+  *   0   1   2   3   4   5   6
+  * |___|___|___|___|___|___|___|
+  * |_X_|___|___|___|___|___|___|
+  * |_O_|___|___|___|_X_|___|___|
+  * |_X_|___|___|_O_|_O_|___|___|
+  * |_X_|___|_X_|_O_|_O_|_X_|_O_|
+  * |_X_|___|_X_|_O_|_O_|_O_|_X_|
+  *
+  */
+
+  "The gamefield" should
+    "return '' as winner in this complex match" in {
+    reset
+    gameField.dropCoin(0, opponent)
+    gameField.dropCoin(0, opponent)
+    gameField.dropCoin(0, opponent)
+    gameField.dropCoin(0, player)
+    gameField.dropCoin(0, opponent)
+
+    gameField.dropCoin(2, opponent)
+    gameField.dropCoin(2, opponent)
+
+    gameField.dropCoin(3, player)
+    gameField.dropCoin(3, player)
+    gameField.dropCoin(3, player)
+
+    gameField.dropCoin(4, player)
+    gameField.dropCoin(4, player)
+    gameField.dropCoin(4, player)
+    gameField.dropCoin(4, opponent)
+
+    gameField.dropCoin(5, player)
+    gameField.dropCoin(5, opponent)
+
+    gameField.dropCoin(6, opponent)
+    gameField.dropCoin(6, player)
+
+    gameField.getWinner should be("")
+  }
 }
