f2ec7ebe820c1432367a676a31f34cd377c444f8
Markus Harr <maharr@htwg-konstanz.de>
2015-12-25 13:57:46 +0100

    Connect4GameField more functional

--- src/main/scala/connectfour/model/Connect4GameField.scala
+++ src/main/scala/connectfour/model/Connect4GameField.scala
@@ -16,14 +16,29 @@ class Connect4GameField(player1: Player, player2: Player) {
   private var winner: Player = _
 
   //TODO var
-  protected var playerOnTurn: Player = {
+  //  protected var playerOnTurn: Player = {
+  //    if (Math.random() < 0.5)
+  //      player1
+  //    else
+  //      player2
+  //  }
+
+  def initPlayerOnTurn: Player = {
     if (Math.random() < 0.5)
       player1
     else
       player2
   }
 
-  def getPlayerOnTurn: Player = playerOnTurn
+  def getPlayerOnTurn(currentPlayer: Player): Player = {
+    //playerOnTurn
+    if (currentPlayer == player1)
+      player1
+    else if (currentPlayer == player2)
+      player2
+    else
+      throw new IllegalStateException("No player is on turn!")
+  }
 
   def getWinner = {
     if (winner == null) {
@@ -33,20 +48,11 @@ class Connect4GameField(player1: Player, player2: Player) {
     }
   }
 
-  def changePlayerOnTurn() = {
-    if (playerOnTurn == player1)
-      playerOnTurn = player2
-    else if (playerOnTurn == player2)
-      playerOnTurn = player1
-    else
-      throw new IllegalStateException("No player is on turn!")
-  }
-
-  def dropCoin(column: Int): Boolean = {
+  def dropCoin(currentPlayer: Player, column: Int): Player = {
     def insertPlayerInGameField(row: Int) {
       if (row < Connect4GameField.FIELD_ROWS)
         if (gameField(column)(row) == null)
-          gameField(column)(row) = playerOnTurn
+          gameField(column)(row) = getPlayerOnTurn(currentPlayer)
         else
           insertPlayerInGameField(row + 1)
     }
@@ -56,25 +62,47 @@ class Connect4GameField(player1: Player, player2: Player) {
 
     if (Connect4MoveEvaluator.verticalMoveIsPossible(this, column) && winner == null) {
       insertPlayerInGameField(0)
-      
-      if (Connect4MoveEvaluator.playerHasWon(this, playerOnTurn))
-        winner = playerOnTurn
-      
-      changePlayerOnTurn()
-      true
+
+      if (Connect4MoveEvaluator.playerHasWon(this, getPlayerOnTurn(currentPlayer))) {
+        winner = getPlayerOnTurn(currentPlayer)
+      }
+
+      changePlayerOnTurn(currentPlayer)
     } else
-      false
+      null
   }
 
-  def cloneGameField(): Connect4GameField = {
+  private def changePlayerOnTurn(currentPlayer: Player): Player = {
+    if (currentPlayer == player1)
+      player2
+    else if (currentPlayer == player2)
+      player1
+    else
+      throw new IllegalStateException("No player is on turn!")
+  }
+
+  def cloneGameField(currentPlayer: Player): Connect4GameField = {
     val newGameField = new Connect4GameField(player1, player2)
-    newGameField.playerOnTurn = playerOnTurn
+    cloneGameFieldRow(newGameField, 0)
+  }
 
-    //TODO: for
-    for (col <- 0 until Connect4GameField.FIELD_COLUMNS)
-      for (row <- 0 until Connect4GameField.FIELD_ROWS)
-        newGameField.gameField(col)(row) = gameField(col)(row)
+  private def cloneGameFieldRow(newGameField: Connect4GameField, row: Int): Connect4GameField = {
+    if (row < Connect4GameField.FIELD_ROWS) {
+      cloneGameFieldColumn(newGameField, 0, row)
 
-    newGameField
+      cloneGameFieldRow(newGameField, row + 1)
+    } else {
+      newGameField
+    }
+  }
+
+  private def cloneGameFieldColumn(newGameField: Connect4GameField, column: Int, row: Int): Connect4GameField = {
+    if (column < Connect4GameField.FIELD_COLUMNS) {
+      newGameField.gameField(column)(row) = gameField(column)(row)
+
+      cloneGameFieldColumn(newGameField, column + 1, row)
+    } else {
+      newGameField
+    }
   }
 }
